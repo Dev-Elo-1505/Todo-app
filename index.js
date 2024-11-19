951f10e02979c7c6cbefbf2e78e99e72a76f507b
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-11-19 02:38:08 +0100

    fix: mark todo item

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const themeSwitch = document.getElementById("theme-switch");
 
 const createTodoItem = (text) => {
   const li = document.createElement("li");
-  li.innerHTML = `<input type="radio" name="" class="radio-els" />${text}<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
+  li.innerHTML = `<input type="checkbox" name="" class="checkbox-el" />${text}<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
 
-  const radioEl = li.querySelector(".radio-els");
-  if (radioEl.checked) {
+  const checkboxEl = li.querySelector(".checkbox-el");
+  checkboxEl.addEventListener("change", () => {
     li.classList.toggle("completed");
-  }
+  });
 
   const deleteIcon = li.querySelector(".delete-icon");
   deleteIcon.addEventListener("click", () => {
