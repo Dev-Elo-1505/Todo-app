88ce124df4a5901d1eeac97f0022b85ed5e8f092
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-11-19 01:36:58 +0100

    Add todo item

--- index.js
+++ index.js
@@ -3,8 +3,35 @@
 // style with css
 // then tackle javascript
 
-let darkmode = localStorage.getItem("darkmode");
+//logic for adding todo list
+//1 user enters value into input field
+//2 target enter event
+//3 take value from input
+//4 create element with it
+//5 form validation to make sure something is entered
+
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+const radioEl = document.getElementById("radio-el");
 const themeSwitch = document.getElementById("theme-switch");
+
+const createTodoItem = (text) => {
+  const li = document.createElement("li");
+  li.innerHTML = `<input type="radio" name="" id="radio-el" />${text}<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
+  return li;
+};
+
+const addTodo = (event) => {
+  if (event.keyCode === 13 && todoInput.value.trim()) {
+    const todoItem = createTodoItem(todoInput.value);
+    todoUl.appendChild(todoItem);
+    todoInput.value = "";
+  }
+};
+
+todoInput.addEventListener("keypress", addTodo);
+
+let darkmode = localStorage.getItem("darkmode");
 
 const enableDarkMode = () => {
   document.body.classList.add("darkmode");
