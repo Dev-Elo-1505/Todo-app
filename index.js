53d28b04206c6b219dbc6b5f059c9d9c1e7a2352
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-12-04 16:35:53 +0100

    update todo app

--- index.js
+++ index.js
@@ -11,31 +11,48 @@
 //5 form validation to make sure something is entered
 
 const todoInput = document.getElementById("todo-input");
-const todoUl = document.getElementById("todo-ul");
-
+const todoBox = document.getElementById("todo-box");
+const todoInfo = document.getElementById("todo-info");
 const themeSwitch = document.getElementById("theme-switch");
+const todoCountEl = document.getElementById("todo-count");
+const clearCompleted = document.getElementById("clear-completed");
+let todoCount = 0;
 
 const createTodoItem = (text) => {
-  const li = document.createElement("li");
-  li.innerHTML = `<input type="checkbox" name="" class="checkbox-el" />${text}<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
+  const div = document.createElement("div");
+  div.innerHTML = `<div class="todo-in"><input type="checkbox" name="" class="checkbox-el" /><p>${text}</p></div><svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="hsl(236, 9%, 61%)" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
+  div.classList.add("todo-item");
 
-  const checkboxEl = li.querySelector(".checkbox-el");
+  todoInfo.style.display = "flex";
+  todoCount++;
+  todoCountEl.textContent = todoCount;
+  const checkboxEl = div.querySelector(".checkbox-el");
   checkboxEl.addEventListener("change", () => {
-    li.classList.toggle("completed");
+    div.classList.toggle("completed");
+    todoCount--;
+    todoCountEl.textContent = todoCount;
   });
 
-  const deleteIcon = li.querySelector(".delete-icon");
+  const deleteIcon = div.querySelector(".delete-icon");
   deleteIcon.addEventListener("click", () => {
-    li.remove();
+    div.remove();
+    todoCount--;
+    todoCountEl.textContent = todoCount;
   });
 
-  return li;
+  clearCompleted.addEventListener("click", () => {
+    if (div.classList.contains("completed")) {
+      div.remove();
+    }
+  });
+
+  return div;
 };
 
 const addTodo = (event) => {
   if (event.keyCode === 13 && todoInput.value.trim()) {
     const todoItem = createTodoItem(todoInput.value);
-    todoUl.appendChild(todoItem);
+    todoBox.appendChild(todoItem);
     todoInput.value = "";
   }
 };
